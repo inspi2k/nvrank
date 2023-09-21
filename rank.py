1a81e917a7901a684478fa9d0ef01dd925bd46a2
inspi2k <inspi@hanmail.net>
2023-09-21 20:18:50 +0900

    add readme

--- rank.py
+++ rank.py
@@ -1,19 +1,3 @@
-# 230919 화 오후5
-# git 연동 -> Heroku(Cancel) -> Koyeb 업로드
-#   - 1분마다 인스턴스 생성되어 돌아가고 있음 (스케쥴 관리 따로 없음)
-#   -> 스케쥴 cron 찾기
-#   -> Django Project 로 웹에서 관리하고,
-
-# 230905 화 오후2
-# 검색페이지 api의 json은 권한 오류로 사용불가
-# 대체 - catalog 상품은 제목으로 찾기
-# 구글 시트에 기록
-
-# 230904 월
-# 네이버 검색 API 이용하여 순위 찾기
-# - 28페이지 29위까지 검색가능 (1000위부터 100개까지)
-# 구글 시트에서 읽기
-
 import datetime
 import urllib.request
 import json
